268cb94e41ae0653ac3f16aa582c581b506c1ece
Thierry AGNELLI <thierry.agnelli@gmail.com>
2023-03-10 06:35:31 +0000

    feat: adding legacy config to nodemon script for working in devContainer Refs: #MYM-6

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(logger("dev"));
 app.use("/", (req: Request, res: Response) =>
   res.status(200).json({
-    answer: "response!",
+    answer: "response!!!!!",
     body: req.body ? req.body : "none",
   })
 );
