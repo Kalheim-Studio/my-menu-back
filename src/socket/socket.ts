5e736e488e5b8a45088d734ef8dcbaf3bc09d6aa
Thierry AGNELLI <thierry.agnelli@gmail.com>
2023-03-16 20:09:27 +0100

    fix: lint code Resfs: #MYM-8

--- src/socket/socket.ts
+++ src/socket/socket.ts
@@ -2,41 +2,41 @@ import type { Server } from "socket.io";
 import authenticate from "./Middlewares/authentication";
 
 const socket = (io: Server) => {
-  authenticate(io);
+    authenticate(io);
 
-  io.on("connection", (socket) => {
-    console.log("Socket connexion");
+    io.on("connection", (socket) => {
+        console.log("Socket connexion");
 
-    // get restaurant id
-    // Join restaurant id room
-    socket.join("restaurantId");
+        // get restaurant id
+        // Join restaurant id room
+        socket.join("restaurantId");
 
-    // Listening event
-    // Change table state
-    socket.on("tableState", (data) => {
-      console.log("Table state - " + data.content);
-      io.to("restaurantId").emit("message", {
-        content: "Table State received",
-        username: "MyMenu",
-      });
+        // Listening event
+        // Change table state
+        socket.on("tableState", (data) => {
+            console.log("Table state - " + data.content);
+            io.to("restaurantId").emit("message", {
+                content: "Table State received",
+                username: "MyMenu",
+            });
+        });
+        // Waiter help table state
+        socket.on("helpState", (data) => {
+            console.log("Table Waiter help state - " + data.content);
+            io.to("restaurantId").emit("message", {
+                content: "Help State received",
+                username: "MyMenu",
+            });
+        });
+        // Launch order
+        socket.on("launchOrder", (data) => {
+            console.log("Launch Order - " + data.content);
+            io.to("restaurantId").emit("message", {
+                content: "Launch Order received",
+                username: "MyMenu",
+            });
+        });
     });
-    // Waiter help table state
-    socket.on("helpState", (data) => {
-      console.log("Table Waiter help state - " + data.content);
-      io.to("restaurantId").emit("message", {
-        content: "Help State received",
-        username: "MyMenu",
-      });
-    });
-    // Launch order
-    socket.on("launchOrder", (data) => {
-      console.log("Launch Order - " + data.content);
-      io.to("restaurantId").emit("message", {
-        content: "Launch Order received",
-        username: "MyMenu",
-      });
-    });
-  });
 };
 
 export default socket;
