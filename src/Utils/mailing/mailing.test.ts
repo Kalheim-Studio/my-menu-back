6d0beae40dccbe7fd0ef2fab69037a09ace10aa5
Thierry AGNELLI <thierry.agnelli@gmail.com>
2023-04-13 07:46:54 +0000

    fix: Temporary desactivating pipelines. Refs: #MYM-10

--- src/Utils/mailing/mailing.test.ts
+++ src/Utils/mailing/mailing.test.ts
@@ -1,4 +1,5 @@
 import { sendAccountValidationMail } from "./mailing";
+import nodemailer from "nodemailer";
 import dotenv from "dotenv";
 
 describe("testing mailing methods", () => {
@@ -14,24 +15,23 @@ describe("testing mailing methods", () => {
         expect(result).toBe("Email sent");
     });
 
-    it.todo("Should throw error");
-    // it("Should throw error", async () => {
-    //     const email = "thierry.agnelli@gmail.com";
-    //     const name = "John Doe";
-    //     const token = "thisisatoken";
+    it("Should throw error", async () => {
+        const email = "thierry.agnelli@gmail.com";
+        const name = "John Doe";
+        const token = "thisisatoken";
 
-    //     const result = await sendAccountValidationMail(email, name, token, () =>
-    //         nodemailer.createTransport({
-    //             host: String(process.env.MAIL_HOST),
-    //             port: parseInt(String(process.env.MAIL_PORT)),
-    //             secure: true,
-    //             auth: {
-    //                 user: String(process.env.EMAIL),
-    //                 pass: "wrong_pwd",
-    //             },
-    //         })
-    //     );
-    //     // const result = await sendAccountValidationMail(email, name, token);
-    //     expect(result).toBe("Error on mail sending");
-    // });
+        const result = await sendAccountValidationMail(email, name, token, () =>
+            nodemailer.createTransport({
+                host: String(process.env.MAIL_HOST),
+                port: parseInt(String(process.env.MAIL_PORT)),
+                secure: true,
+                auth: {
+                    user: String(process.env.EMAIL),
+                    pass: "wrong_pwd",
+                },
+            })
+        );
+        // const result = await sendAccountValidationMail(email, name, token);
+        expect(result).toBe("Error on mail sending");
+    });
 });
