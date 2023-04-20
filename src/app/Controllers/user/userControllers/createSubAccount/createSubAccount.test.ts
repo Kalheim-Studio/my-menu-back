e2e5d8ef247462864519b967295bf9fd5c69ded2
Thierry Agnelli <thierry.agnelli@gmail.com>
2023-04-20 08:11:03 +0000

    fix: createSubAccount test fix. Refs: #MYM-13

--- src/app/Controllers/user/userControllers/createSubAccount/createSubAccount.test.ts
+++ src/app/Controllers/user/userControllers/createSubAccount/createSubAccount.test.ts
@@ -1,11 +1,21 @@
 import dotenv from "dotenv";
 import mongoose from "mongoose";
-import type { Request } from "express";
+import type { Request, Response, NextFunction } from "express";
 import { v4 as uuid } from "uuid";
 import request from "supertest";
 import app from "../../../../app";
 import jwt from "jsonwebtoken";
 import { User } from "../../../../Models/User";
+import checkAuth from "../../../../Middlewares/checkAuth/checkAuth";
+import checkData from "../../../../Middlewares/checkData/checkData";
+
+// Mocking checkAuth Middleware
+jest.mock("../../../../Middlewares/checkAuth/checkAuth", () =>
+    jest.fn((req: Request, res: Response, next: NextFunction) => next()));
+
+// Mocking checkData Middleware
+jest.mock("../../../../Middlewares/checkData/checkData", () =>
+    jest.fn((req: Request, res: Response, next: NextFunction) => next()));
 
 describe("createSubAccount controller test", () => {
     dotenv.config();
@@ -42,11 +52,15 @@ describe("createSubAccount controller test", () => {
     });
 
     it("Should response error if authentication failed", async () => {
+        
+        // Mocking once error on checkAuth middleware
+        (checkAuth as jest.Mock).mockImplementationOnce((req: Request, res: Response) => res.status(400).send("Authentication failed") )
+
         const response = await request(app)
             .post("/user/create-sub-account")
             .set("token", "thisisabadtoken")
             .send(req.body);
-
+        
         // Search for entry in database
         const userResult = await User.findOne({ identifier: "John_Doe_" + uid });
 
@@ -57,7 +71,8 @@ describe("createSubAccount controller test", () => {
     });
 
     it("Should response error if data are not valid", async () => {
-        req.body.role = 1;
+        // Mocking once error on checkData middleware
+        (checkData as jest.Mock).mockImplementationOnce((req: Request, res: Response) => res.status(400).send("Error: Data are not valid.") )
 
         const response = await request(app)
             .post("/user/create-sub-account")
